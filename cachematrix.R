c004dfdae5df9fd27ac82ce13ed438874e407556
prhorridge <p.horridge@signal.qinetiq.com>
2014-07-18 14:32:14 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,29 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(thematrix = numeric()) {
+	inverse <- NULL
+	set <- function(matrix_in) {
+		thematrix <<- matrix_in   # set new matrix to invert
+		inverse <<- NULL          # reset the inverse since we need to recalculate
+	}
+	get <- function() thematrix   # return the matrix
+	setinverse <- function(inv_in) inverse <<- inv_in # set the inverse
+	getinverse <- function() inverse                  # return the inverse
+	list(set = set, get = get, setinverse = setinverse,
+		getinverse = getinverse)
 }
-
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cachedMatrix,...) {
+	inv <- cachedMatrix$getinverse()
+	if(!is.null(inv)) {
+		message("getting cached inverse")
+		return(inv)
+	}
+	thematrix <- cachedMatrix$get()
+	inv <- solve(thematrix,...)
+	cachedMatrix$setinverse(inv)
+	inv
 }
